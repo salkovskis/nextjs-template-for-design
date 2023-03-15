8982f2677a1eb670b6de23d28327b6bc183f9b11
Raivo <raivo@salkovskis.com>
2023-03-15 17:02:00 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const IndexPage = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="text-gray-700 text-4xl font-light border-b border-gray-400">
+        Base app
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
